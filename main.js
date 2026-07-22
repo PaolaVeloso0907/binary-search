142c56dd9e22e574c638996f1ff1c2465d955f40
PaolaVeloso0907 <paola.paula@sptech.school>
2026-07-22 16:11:09 -0300

    feat: Inicia o projeto

---
+++ main.js
@@ -0,0 +1,3 @@
+// 1. Gerar série de números aleatórios e exibir na tela (Math.random)
+// 2. Clicra em um botão e ordenar os números. (logica da amarelinha (ordenação linear))
+// 3. Digitar um número e buscar a posição dele entre os numeros. (Binary Search)
